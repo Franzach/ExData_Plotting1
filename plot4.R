24d7458782167f86a81481409491870f52b6f541
Franzach <sakstony@gmail.com>
2015-02-08 20:47:05 +0100

    Plots 3 & 4
    Update to code and charts for plots 3 & 4

--- plot4.R
+++ plot4.R
@@ -16,14 +16,32 @@ consdata1$Sub_metering_1 <- as.numeric(as.character(consdata1$Sub_metering_1))
 consdata1$Sub_metering_2 <- as.numeric(as.character(consdata1$Sub_metering_2))
 consdata1$Sub_metering_3 <- as.numeric(as.character(consdata1$Sub_metering_3))
 
-# Plot the data
-png(filename = "plot3.png")
+consdata1$Global_reactive_power <- as.numeric(as.character(consdata1$Global_reactive_power))
+consdata1$Voltage <- as.numeric(as.character(consdata1$Voltage))
+
+# Plot the data - 4 charts total 2 x 2 by row
+png(filename = "plot4.png")
+par(mfrow = c(2, 2))
+
+# 1 Global Active Power plot
+plot(x = consdata1$Time, y = consdata1$Global_active_power, type = "l", 
+     xlab = "", ylab = "Global Active Power")
+
+# 2 Voltage plot
+plot(x = consdata1$Time, y = consdata1$Voltage, type = "l", 
+     xlab = "datetime", ylab = "Voltage")
+
+# 3 Sub metering plot
 plot(x = consdata1$Time, y = consdata1$Sub_metering_1, type = "n", 
      xlab = "", ylab = "Energy sub metering")
 points(x = consdata1$Time, y = consdata1$Sub_metering_1, type = "S", col = "black")
 points(x = consdata1$Time, y = consdata1$Sub_metering_2, type = "S", col = "red")
 points(x = consdata1$Time, y = consdata1$Sub_metering_3, type = "S", col = "blue")
-legend("topright", lty = 1, lwd = 1, col = c("black", "red", "blue"), 
+legend("topright", bty = "n", lty = 1, lwd = 1, col = c("black", "red", "blue"), 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-dev.off()
 
+# 4 Global reactive Power plot
+plot(x = consdata1$Time, y = consdata1$Global_reactive_power, type = "l", 
+          xlab = "datetime", ylab = "Global_reactive_Power")
+
+dev.off()
